6ba008392d6a3a04f2fc79161581f9d98aecbdcd
Sven Primus <svenprimus@gmail.com>
2026-07-17 01:53:05 +0200

    feat: hero fully functional, header / footer improved

--- script.js
+++ script.js
@@ -9,6 +9,33 @@ function toggleMenu() {
     }
 }
 
-// function renderBasketCount() 
+// function renderBasketCount()
 // id="button-basket-overlay"  d_none
 // id="button-basket-counter"
+
+function toggleLike() {
+    renderLikedButton();
+    renderLikedCount();
+}
+
+function renderLikedButton() {
+    const buttonRef = document.getElementById('button-star');
+    if (buttonRef.classList.contains('button-star-liked')) {
+        buttonRef.classList.add('button-star-default');
+        buttonRef.classList.remove('button-star-liked');
+    } else {
+        buttonRef.classList.add('button-star-liked');
+        buttonRef.classList.remove('button-star-default');
+    }
+}
+
+function renderLikedCount() {
+    const buttonRef = document.getElementById('button-star');
+    const counterRef = document.getElementById('rating-counter');
+
+    if (buttonRef.classList.contains('button-star-liked')) {
+        counterRef.innerHTML = Number(counterRef.innerHTML) + 1;
+    } else {
+        counterRef.innerHTML = counterRef.innerHTML - 1;
+    }
+}
